f121fa7df7c21783af386ba8262ed88f7cc3ac32
Kim Le <kimthn.le@gmail.com>
2020-10-15 17:19:33 -0400

    Smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,20 @@
     (function($) {
     "use strict"; // Start of use strict
 
+    $(document).ready(function() {
+        $("a").on('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 500, function() {
+                    window.location.hash = hash;
+                });
+            }
+        });
+    });
+    
     // Scroll to top button appear
     $(document).scroll(function() {
       var scrollDistance = $(this).scrollTop();
